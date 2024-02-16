007d7246bb44939d06ca78a21c57365de3bcb498
afulle12 <bhfwz5fg1@mozmail.com>
2024-02-15 23:46:30 -0700

    Minor changes

--- Add-Subtract-Multiply-Divide/main.c
+++ Add-Subtract-Multiply-Divide/main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
-int value, a, b;
+int a, b;
+int getInput();
 
 int main(){
     char operation[20];
@@ -9,25 +10,21 @@ int main(){
     printf("Do you want to Add, Subtract, Multiply, or Divide? ");
     scanf("%s", operation);
 
-    //value = strcmp(operation, "Add");
     if(strcmp(operation, "Add") == 0){
         getInput();
         printf("The result is %d\n", a+b);
     }
 
-    //value = strcmp(operation, "Subtract");
     else if(strcmp(operation, "Subtract") == 0){
         getInput();
         printf("The result is: %d\n", a-b);
     }
 
-    //value = strcmp(operation, "Multiply");
     else if(strcmp(operation, "Multiply") == 0){
         getInput();
         printf("The result is: %d\n", a*b);
     }
 
-    //value = strcmp(operation, "Divide");
     else if(strcmp(operation, "Divide") == 0){
         getInput();
         printf("The result is: %d\n", a/b);
@@ -42,7 +39,9 @@ int main(){
 int getInput(){
     printf("Please input the first number: \n");
     scanf("%d", &a);
+
     printf("Please input the second number: \n");
     scanf("%d", &b);
+    
     return(a, b);
 }
